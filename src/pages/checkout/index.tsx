08d9ca441e6ea7db638ba6da08760bc40f683f59
Isaac Aguiar <isaacaguiar_@hotmail.com>
2022-09-05 05:55:54 -0300

    Estilização checkout v1.3

--- src/pages/checkout/index.tsx
+++ src/pages/checkout/index.tsx
@@ -1,6 +1,6 @@
-import { FormContainer, Frame1, Frame2 } from "./style";
+import { FormContainer, Frame1, Frame2, HeaderFrame1, InputGroup, PaymentMethods } from "./style";
 import { MiniCard } from "../../components/MiniCard";
-import { MapPinLine } from "phosphor-react";
+import { Bank, CreditCard, CurrencyDollar, MapPinLine, Money } from "phosphor-react";
 
 export function Checkout() {
     return (
@@ -9,30 +9,46 @@ export function Checkout() {
             <div>
               <h4>Complete seu pedido</h4>
               <Frame1>
-                <span><MapPinLine size={24}/> <p>Endereço de Entrega</p></span>
-                <p>Informe o endereço onde deseja receber o pedido</p>
+
+                <HeaderFrame1>
+                  <MapPinLine size={24}/>
+                  <div>
+                    <p>Endereço de Entrega</p>
+                    <p>Informe o endereço onde deseja receber o pedido</p>
+                  </div>
+                </HeaderFrame1>
                 
-                <input type="text" placeholder="CEP" name="CEP" id="CEP" />
-                <input type="text" placeholder="Rua" name="RUA" id="RUA" />
-                <input type="text" placeholder="Número" name="NUM" id="NUM" />
-                <input type="text" placeholder="Complemento" name="COMP" id="COMP"/>
-                <input type="text" placeholder="Bairro" name="BAIRRO" id="BAIRRO" />
-                <input type="text" placeholder="Cidade" name="CIDADE" id="CIDADE" />
-                <input type="text" placeholder="UF" name="UF" id="UF" />
+                <InputGroup>
+                  <input type="text" placeholder="CEP" name="CEP" id="CEP" />
+                  <input type="text" placeholder="Rua" name="RUA" id="RUA" />
+
+                  <div>
+                    <input type="text" placeholder="Número" name="NUM" id="NUM" />
+                    <input type="text" placeholder="Complemento" name="COMP" id="COMP"/>  
+                  </div>
+
+                  <div>
+                    <input type="text" placeholder="Bairro" name="BAIRRO" id="BAIRRO" />
+                    <input type="text" placeholder="Cidade" name="CIDADE" id="CIDADE" />
+                    <input type="text" placeholder="UF" name="UF" id="UF" />
+                  </div>
+                </InputGroup>
               </Frame1>
 
-              <div>
-                <span>Pagamento</span>
+              
+              <PaymentMethods>
+                <span className="titlePayment"><CurrencyDollar size={24}/>Pagamento</span>
                 <p>O pagamento é feito na entrega. Escolha a forma que desejar pagar</p>
 
                 <div>
-                  <span>CARTÃO DE CRÉDITO</span>
-                  <span>CARTÃO DE DÉBITO</span>
-                  <span>DINHEIRO</span>
+                  <span><CreditCard size={24}/>CARTÃO DE CRÉDITO</span>
+                  <span><Bank size={24} />CARTÃO DE DÉBITO</span>
+                  <span><Money size={24} />DINHEIRO</span>
                 </div>
-              </div>
-            </div>
 
+              </PaymentMethods>
+            </div>
+            
             <div>
               <h4>Cafés selecionados</h4>
 
@@ -40,6 +56,23 @@ export function Checkout() {
                 <Frame2>
                   <MiniCard />
                   <MiniCard />
+
+                  <div>
+                    <p>Total de itens</p>
+                    <p>R$ 29,90</p>
+                  </div>
+
+                  <div>
+                    <p>Entrega</p>
+                    <p>R$ 3,50</p>
+                  </div>
+
+                  <div>
+                    <strong>Total</strong>
+                    <strong>R$ 33,20</strong>
+                  </div>
+
+                  <button type="submit">Confirmar Pedido</button>
                 </Frame2>
               </div>
             </div>
--- src/pages/checkout/style.ts
+++ src/pages/checkout/style.ts
@@ -2,6 +2,15 @@ import styled from 'styled-components'
 
 export const FormContainer = styled.form`
   display: flex;
+  gap: 2rem;
+
+  h4 {
+    margin-bottom: 2rem;
+    font-family: 'Baloo 2';
+    font-weight: bold;
+    /* Base/Subtitle */
+    color: #403937;
+  }
 `
 
 export const Frame1 = styled.div`
@@ -12,17 +21,132 @@ export const Frame1 = styled.div`
   background: #F3F2F2;
   border-radius: 6px;
 
+  `
+
+export const HeaderFrame1 = styled.div`
+  display: flex;
+  gap: 0.5rem;
+  
+  svg {
+    color: #C47F17;
+  }
+
+  div p:nth-child(1){
+  /* Base/Subtitle */
+  color: #403937;
+  font-weight: bold;
+  margin-bottom: 0.5rem;
+  }
+`
+
+export const InputGroup = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+
+  div {
+    display: flex;
+    gap: 0.75rem;
+  }
+
+  input {
+    padding: 12px;
+    /* Base/Input */
+    background: #EEEDED;
+    /* Base/Button */
+    border: 1px solid #E6E5E5;
+    border-radius: 4px;
+    /* Base/Label */
+    color: #8D8686;
+  }
+
+  input#CEP {
+    width: 12.5rem;
+    margin-top: 2rem;
+  }
+
+  input#RUA {
+    width: 21.75rem;
+  }
+
+  input#NUM, input#BAIRRO {
+    width: 12.5rem;
+  }
+
+  input#COMP {
+    width: 100%
+  }
+
+  input#CIDADE {
+    width: 18rem;
+  }
+
+  input#UF {
+    width: 3.75rem;
+  }
+`
+
+
+export const PaymentMethods = styled.div`
+  margin-top: 0.75rem;
+  padding: 2.5rem;
+  /* Base/Card */
+  background: #F3F2F2;
+  border-radius: 6px;
+
   span {
-    display: inline-flex;
+    display: flex;
     align-items: center;
-    gap: 0.5rem;
+  }
+
+  span.titlePayment {
+    /* Base/Subtitle */
+    color: #403937;
+    font-weight: bold;
+    line-height: 130%;
+  }
+
+  p {
+    margin-left: 1.35rem;
+    margin-bottom: 2rem;
   }
 
   span svg {
-    color: #C47F17;
+    /* Brand/Purple */
+    color: #8047F8;
+  }
+
+  div {
+    display: flex;
+    gap: 0.75rem;
+  }
+
+  div span {
+    /* Base/Button */
+    gap: 0.75rem;
+    background: #E6E5E5;
+    border-radius: 6px;
+    width: 11.12rem;
+    height: 3.18rem;
+    padding: 1rem;
+
+    font-size: 10px;
+    line-height: 160%;
+    /* or 19px */
+
+    text-transform: uppercase;
+
+    /* Base/Text */
+
+    color: #574F4D;
+
   }
 `
 
 export const Frame2 = styled.div`
   width: 28rem;
+  padding: 2.5rem;
+  /* Base/Card */
+  background: #F3F2F2;
+  border-radius: 6px 44px;
 `
