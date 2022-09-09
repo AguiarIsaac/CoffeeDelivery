e57492c3984029ce32367c7664998255bc8179c5
Isaac Aguiar <37755163+AguiarIsaac@users.noreply.github.com>
2022-09-09 13:03:33 +0000

    layout page success

--- src/pages/success/index.tsx
+++ src/pages/success/index.tsx
@@ -1,8 +1,35 @@
+import { CurrencyDollar, MapPin, Timer } from "phosphor-react";
+import { Title, ContentInfo, OrderInfo, Infos } from "./style";
+import img from  '../../assets/img/Illustration.svg'
 
 export function Success() {
     return (
         <>
-          <h1>Success</h1>
+          <Title>
+            <h2>Uhu! Pedido confirmado</h2>
+            <p>Agora é só aguardar que logo o café chegará até você</p>
+          </Title>
+
+          <ContentInfo>
+            <OrderInfo>
+              <Infos>
+                <MapPin id="Map" />
+                <p>Entrega em <strong>Rua João Daniel Martinelli, 102</strong><br/>Farrapos - Porto Alegre, RS</p>
+              </Infos>
+
+              <Infos>
+                <Timer id="Timer"/>
+                <p>Previsão de entrega<br/><strong>20 min - 30 min</strong></p>
+              </Infos>
+
+              <Infos>
+                <CurrencyDollar id="Dolar"/>
+                <p>Pagamento na entrega<br/><strong>Cartão de crédito</strong></p>
+              </Infos>
+            </OrderInfo>
+
+            <img src={img} alt="Entregador de cafés em uma moto" />
+          </ContentInfo>
         </>
     )
 }
---
+++ src/pages/success/style.ts
@@ -0,0 +1,86 @@
+import styled from 'styled-components'
+
+export const Title = styled.div`
+  margin-top: 5rem;
+  margin-bottom: 2.5rem;
+
+  h2 {
+    color: ${props => props.theme.colors.yellowDark};
+    font-family: 'Baloo 2';
+    font-style: normal;
+    font-weight: 800;
+    font-size: 32px;
+    line-height: 130%;
+  }
+
+  p {
+    font-weight: 400;
+    font-size: 20px;
+    line-height: 130%;
+    color: ${props => props.theme.colors.baseSubtitle};
+    
+  }
+`
+
+export const ContentInfo = styled.div`
+ display: flex;
+ justify-content: center;
+ align-items: center;
+ gap: 6.375rem;
+ flex-wrap: wrap;
+`
+
+export const OrderInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+  min-width: 32.875rem;
+  
+  padding: 2.5rem;
+  position: relative;
+  background: ${props => props.theme.colors.backgorund};
+  border-radius: 6px 32px;
+
+  &::after {
+    content: "";
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    z-index: -1;
+    margin: -1px;
+    border-radius: inherit;
+    background-image: linear-gradient(77deg, rgba(219,172,44,1) 0%, rgba(128,71,248,1) 100%);
+  }
+`
+
+export const Infos = styled.div`
+ display: flex;
+ gap: 0.75rem;
+ align-items: center;
+
+ font-size: 16px;
+ line-height: 130%;
+ color: ${props => props.theme.colors.baseText};
+
+ svg {
+      width: 2rem;
+      height: auto;
+      border-radius: 50%;
+      padding: 8px;
+      color: ${props => props.theme.colors.backgorund};
+    }
+
+  svg#Map {
+    background: ${props => props.theme.colors.purple};
+  }
+
+  svg#Timer {
+    background: ${props => props.theme.colors.yellow};
+  }
+
+  svg#Dolar {
+    background: ${props => props.theme.colors.yellowDark};
+  }
+`
